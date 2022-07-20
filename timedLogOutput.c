110d70a677c951296df1a509b8d5a5ba7ecf0dc6
screenshifter <screenshifter@gmail.com>
2022-07-20 15:34:43 +0200

    Added nessage printing with timer timeout

--- timedLogOutput.c
+++ timedLogOutput.c
@@ -3,6 +3,7 @@
 #include <linux/timer.h>
 
 static struct timer_list _timer;
+static uint64_t _timerMessageCounter = 0;
 
 
 static void timer_callback(struct timer_list *timer);
@@ -11,11 +12,11 @@ static void timer_callback(struct timer_list *timer);
 int timedLogOutput_printMessages(int amount, char *message)
 {
 	timer_setup(&_timer, timer_callback, 0);
+
+	_timerMessageCounter = amount;
 	
-	// Segmentation fault somewhere here
-	
-	//mod_timer(&_timer, 100);
-	//add_timer(&_timer);
+	add_timer(&_timer);
+	mod_timer(&_timer, 100);
 
 	return 0;
 }
@@ -24,4 +25,12 @@ int timedLogOutput_printMessages(int amount, char *message)
 static void timer_callback(struct timer_list *timer)
 {
 	pr_info("Kernel timer expired\r\n");
+
+	if(_timerMessageCounter > 0)
+		_timerMessageCounter--;
+		
+	if(_timerMessageCounter > 0)
+		add_timer(timer);
+
+	return;
 }
