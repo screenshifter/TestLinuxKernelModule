419795e9e5f8da3d5f988eb1a99f505b6c8f9505
screenshifter <screenshifter@gmail.com>
2022-07-21 14:29:33 +0200

    Message passed to log output function is printed

--- timedLogOutput.c
+++ timedLogOutput.c
@@ -1,5 +1,6 @@
 #include "timedLogOutput.h"
 
+#include <linux/string.h>
 #include <linux/timer.h>
 
 
@@ -8,6 +9,7 @@ static const uint32_t _timerJiffiesPerSecond = 250;
 
 static struct timer_list _timer;
 static uint64_t _timerMessageCounter = 0;
+static char _message[256];
 
 
 static void timerCallback(struct timer_list *timer);
@@ -18,6 +20,16 @@ bool timedLogOutput_printMessages(int amount, char *message)
 {
 	timer_setup(&_timer, timerCallback, 0);
 
+	if(strlen(message) > sizeof(_message))
+	{
+		pr_info("%s: message is too large, restriction is %u bytes\r\n", __func__, sizeof(_message));
+		strcpy(_message, "Kernel timer expired");
+	}
+	else
+	{
+		strcpy(_message, message);
+	}
+
 	_timerMessageCounter = amount;
 	return messageQueue();
 }
@@ -25,7 +37,7 @@ bool timedLogOutput_printMessages(int amount, char *message)
 
 static void timerCallback(struct timer_list *timer)
 {
-	pr_info("Kernel timer expired\r\n");
+	pr_info("%s: %s\r\n", __func__, _message);
 	
 	if(messageQueue() == false)
 		pr_info("%s: all messages are printed\r\n", __func__);
